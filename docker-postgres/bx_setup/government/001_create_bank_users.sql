015c3ecd1803f8c2fbd31d15c5115ca7dd8e106b
miyake <miyake.kouta@ist.osaka-u.ac.jp>
2019-12-10 16:47:31 +0900

    save

--- docker-postgres/bx_setup/government/001_create_bank_users.sql
+++ docker-postgres/bx_setup/government/001_create_bank_users.sql
@@ -6,11 +6,3 @@ CREATE TABLE government_users (
   ADDRESS	varchar(80),
   BIRTHDATE	varchar(80)
 );
-
-CREATE VIEW dejima_bank (
-  ID	int,
-  FIRST_NAME	varchar(80),
-  LAST_NAME	varchar(80),
-  PHONE	varchar(80),
-  ADDRESS	varchar(80)
-);
