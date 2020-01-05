061e797661e3469e4c32315ac2b65f0010587669
miyake <miyake.kouta@ist.osaka-u.ac.jp>
2020-01-06 05:42:37 +0900

    add test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+./request.sh bank 1
+./request.sh bank 2
+./request.sh bank 3
+./request.sh bank 4
+./request.sh bank 5
+./request.sh bank 6
+./request.sh bank 7
+./request.sh bank 8
+./request.sh bank 9
+./request.sh bank 10
